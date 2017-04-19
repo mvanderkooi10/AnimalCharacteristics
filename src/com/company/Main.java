074c0c0226f7b33f1108c971d4e31075ba80a8b9
mvanderkooi10 <mvanderkooi10@gmail.com>
2017-04-19 17:53:06 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Animal animal = new Animal("Animal",1,1,5,5);
+
+	    Dog dog = new Dog("Mutt",5,50,2,4,1,28,"Short");
+       // dog.eat();
+        dog.walk();
+       // dog.run();
+
+    }
+}
